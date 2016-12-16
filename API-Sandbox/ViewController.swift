6a50d33d63d02c7a3c91c9c89e9244499954e1d1
Mehmet Sadıç <mehmetsadic@Mehmet-iMac.local>
2016-12-16 22:50:36 +0300

    Make first working API. Showing one message only

--- API-Sandbox/ViewController.swift
+++ API-Sandbox/ViewController.swift
@@ -18,9 +18,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var numberOfCommentsLabel: UILabel!
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var messageTitleLabel: UILabel!
-    @IBOutlet weak var urlLinkLabel: UILabel!
-  
     
+    var urlLink = ""
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -35,16 +35,16 @@ class ViewController: UIViewController {
                     
                     // Do what you need to with JSON here!
                     // The rest is all boiler plate code you'll use for API requests
-                    let randomMessageNumber = Int(arc4random_uniform(100))
+                    let randomMessageNumber = Int(arc4random_uniform(25))
                     let jsonRedditData = json["data"]["children"][randomMessageNumber]
                     
                     let redditMessage = TurkeyTopics(json: jsonRedditData)
                     
                     self.categoryLabel.text = redditMessage.category
                     self.numberOfCommentsLabel.text = String(redditMessage.numberOfComments)
-                    
+                    self.loadProfileImage(redditMessage.profileImage)
                     self.messageTitleLabel.text = redditMessage.messageTitle
-                    
+                    self.urlLink = redditMessage.link
                     
                 }
             case .Failure(let error):
@@ -52,9 +52,16 @@ class ViewController: UIViewController {
             }
         }
     }
+    
+    func loadProfileImage(urlString: String) {
+        // Updates the image view when passed a url string
+        profileImageView.af_setImageWithURL(NSURL(string: urlString)!)
+    }
   
-  
-  
+  @IBAction func viewOnReddit(sender: UIButton) {
+    // views the original post on reddit site
+    UIApplication.sharedApplication().openURL(NSURL(string: urlLink)!)
+  }
     
 }
 
